a3775a19c3e2974e9410cc3b241aa468d47de7d2
jr-philip <philipmwaura48@gmail.com>
2023-04-17 19:34:19 -0500

    Update App.js
    creating the submit button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,26 @@
+
+import { Form, Field } from 'react-final-form'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Form
+          onSubmit={onSubmit}
+          validate={validate}
+          render={({ handleSubmit }) => (
+            <Form onSubmit={handleSubmit}>
+              <div>
+                <label>First Name</label>
+                <Field name="firstName" component="input" placeholder="First Name" />
+              </div>
+              <button type='submit'>Submit</button>
+            </Form>
+          )
+        }
+        />
+        
         
       </header>
     </div>
