ae074810cc1d7f68415a5e592ddf72c6190d50ea
jr-philip <philipmwaura48@gmail.com>
2023-04-19 17:55:20 -0500

    Update App.js
    creating a classname  for button

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ function App() {
                 <label>First Name</label>
                 <Field name="firstName" component="input" placeholder="First Name" />
               </div>
-              <button type='submit'>Submit</button>
+              <div className='field'>
+                <button type='submit'>Submit</button>
+              </div>
+              
             </form>
           )
         }
